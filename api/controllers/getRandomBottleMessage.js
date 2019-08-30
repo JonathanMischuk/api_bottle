a05fc5dcc3a39e81b6eddb1bd9d5fe7dd98bbcf6
JonathanMischuk <jonathan.mischuk@gmail.com>
2019-08-30 15:31:55 -0400

    removed cruft from controller

--- api/controllers/getRandomBottleMessage.js
+++ api/controllers/getRandomBottleMessage.js
@@ -1,13 +1,7 @@
 const { getRandomBottleMessage } = require('../queries');
 
 module.exports = (req, res) => {
-    // console.log(req.params.name)
-
     getRandomBottleMessage(req.params)
-        .then(response => {
-            // console.log(response);
-
-            res.json(response);
-        })
+        .then(response => res.json(response))
         .catch(err => res.json(err));
 };
